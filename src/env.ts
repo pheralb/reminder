cd960e9b228940bd0f8cdedea43aa4bd8f3f99ba
pheralb <pablohdez4340@gmail.com>
2025-05-10 23:01:52 +0100

    chore: organize files + add shadcn/ui styles

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
   server: {
@@ -7,9 +7,6 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-  },
-  client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,

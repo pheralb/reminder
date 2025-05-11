57b164dcde1872dc9bcc4bfe87038546aecbc1d3
pheralb <pablohdez4340@gmail.com>
2025-05-11 12:08:59 +0100

    feat: add light/dark mode provider + instrument serif font

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,9 +1,17 @@
 import localFont from "next/font/local";
+import { Instrument_Serif } from "next/font/google";
 
 export const fontSans = localFont({
   variable: "--font-sans",
   src: "./InterVariable.woff2",
   weight: "100 900",
   display: "swap",
+  preload: true,
+});
+
+export const instrumentSans = Instrument_Serif({
+  variable: "--font-instrument-sans",
+  display: "swap",
+  weight: "400",
   preload: true,
 });

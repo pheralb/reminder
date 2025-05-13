135ba1b1e22b51dd3d5f749fd5c64c8e9207ed52
pheralb <pablohdez4340@gmail.com>
2025-05-13 18:34:30 +0100

    feat: export all types from schemas + add reminder schema + CRUD for reminder table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -27,20 +27,47 @@ export const collection = createTable("collection", (d) => ({
   id: d.uuid().primaryKey(),
   name: d.text().notNull(),
   colors: d.text(),
+  createdBy: d.text().notNull(),
   organizationId: d
     .uuid()
     .notNull()
     .references(() => organization.id, { onDelete: "cascade" }),
 }));
 
-// Organization & Collection Relations
+// Reminder table
+export const reminder = createTable("reminder", (d) => ({
+  id: d.uuid().primaryKey(),
+  title: d.text().notNull(),
+  description: d.text(),
+  date: d.timestamp({ withTimezone: true }),
+  collectionId: d
+    .uuid()
+    .notNull()
+    .references(() => collection.id, { onDelete: "cascade" }),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  createdBy: d.text().notNull(),
+}));
+
+// Relations
 export const organizationRelations = relations(organization, ({ many }) => ({
   collections: many(collection),
 }));
 
-export const collectionRelations = relations(collection, ({ one }) => ({
+export const collectionRelations = relations(collection, ({ one, many }) => ({
   organization: one(organization, {
     fields: [collection.organizationId],
     references: [organization.id],
+  }),
+  reminders: many(reminder),
+}));
+
+export const reminderRelations = relations(reminder, ({ one }) => ({
+  collection: one(collection, {
+    fields: [reminder.collectionId],
+    references: [collection.id],
   }),
 }));
---
+++ src/server/queries/reminders.ts
@@ -0,0 +1,71 @@
+import type { GetReminders, InsertReminder } from "@/server/db/types";
+
+import { db } from "@/server/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { desc, eq } from "drizzle-orm";
+
+import { reminder } from "@/server/db/schema";
+
+// Get Reminders (by user)
+export const getReminders = async (): Promise<GetReminders[] | null> => {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+  const reminders = await db
+    .select()
+    .from(reminder)
+    .where(eq(reminder.createdBy, user.id))
+    .orderBy(desc(reminder.createdAt));
+
+  return reminders;
+};
+
+// Insert Reminder
+export const insertReminder = async (data: InsertReminder) => {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+  const newReminder = await db
+    .insert(reminder)
+    .values({
+      ...data,
+      createdBy: user.id,
+    })
+    .returning();
+  return newReminder;
+};
+
+// Update Reminder
+export const updateReminder = async (
+  reminderId: InsertReminder["id"],
+  data: Partial<InsertReminder>,
+) => {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+  const updatedReminder = await db
+    .update(reminder)
+    .set({
+      ...data,
+      updatedAt: new Date(),
+    })
+    .where(eq(reminder.id, reminderId))
+    .returning();
+  return updatedReminder;
+};
+
+// Delete Reminder
+export const deleteReminder = async (reminderId: InsertReminder["id"]) => {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+  const deletedReminder = await db
+    .delete(reminder)
+    .where(eq(reminder.id, reminderId))
+    .returning();
+  return deletedReminder;
+};

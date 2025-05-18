ddef26421cf6ae0a8e254fa5fd1c0c5fca8eebc6
pheralb <pablohdez4340@gmail.com>
2025-05-18 15:12:16 +0100

    feat: add update & delete organization server functions

--- src/server/queries/organizations.ts
+++ src/server/queries/organizations.ts
@@ -4,6 +4,8 @@ import type { InsertOrganization } from "@/server/db/types";
 
 import { db } from "@/server/db";
 import { organization } from "@/server/db/schema";
+
+import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { currentUser } from "@clerk/nextjs/server";
 
@@ -26,4 +28,45 @@ export const insertOrganization = async (
   revalidatePath("/");
 
   return newOrganization;
+};
+
+// Update Organization
+export const updateOrganization = async (
+  orgId: string,
+  orgData: InsertOrganization,
+): Promise<InsertOrganization[] | null> => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const updatedOrganization = await db
+    .update(organization)
+    .set({
+      ...orgData,
+      createdBy: user.id,
+    })
+    .where(eq(organization.id, orgId))
+    .returning();
+
+  revalidatePath("/");
+
+  return updatedOrganization;
+};
+
+// Delete Organization
+export const deleteOrganization = async (
+  orgId: string,
+): Promise<boolean | null> => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  await db
+    .delete(organization)
+    .where(and(eq(organization.id, orgId), eq(organization.createdBy, user.id)))
+    .returning();
+
+  revalidatePath("/");
+
+  return true;
 };

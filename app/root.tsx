8c9c8c9c10a0ada2f8d07970e12ffb96c1b326f3
pheralb <pablohdez4340@gmail.com>
2025-05-24 21:34:54 +0100

    feat: use fs/routes + organize files

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import type { Route } from "./+types/root";
+
 import {
   isRouteErrorResponse,
   Links,
@@ -7,21 +9,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import type { Route } from "./+types/root";
-import "./app.css";
-
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+// Styles:
+import "@/styles/globals.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
---
+++ app/routes/_index.tsx
@@ -0,0 +1,5 @@
+const Homepage = () => {
+  return <p className="text-2xl font-semibold">Hello</p>;
+};
+
+export default Homepage;

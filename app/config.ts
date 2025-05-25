29745263c916339be9f24aa5860bf4e6ef563043
pheralb <pablohdez4340@gmail.com>
2025-05-25 01:32:09 +0100

    feat: add global app configuration file

---
+++ app/config.ts
@@ -0,0 +1,7 @@
+export const appConfig = {
+  title: "Reminder",
+  description: "A minimalistic web app to remember & organize your things.",
+  prodUrl: "https://reminder.pheralb.dev",
+  cookieProdUrl: "reminder.pheralb.dev",
+  repositoryUrl: "https://github.com/pheralb/reminder",
+};
